457df945c9adbe8a45f8145e8e2c7ce20f784936
rozariw8365 <rozariw8365@kippnyccp.org>
2017-05-15 11:26:56 -0400

    Update 3leaf.js

--- 3leaf.js
+++ 3leaf.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
  $("#name_button").click(function(){
-  var first=
+  var first = $("input[name=instagramname]").val();
+  var last = $("input[name=RequestedMepPart]").val():
+  var response = "ok! here you go," + first + " " + last;
+  $("#ok! here you go,).html(response);
+ }];
+ $
