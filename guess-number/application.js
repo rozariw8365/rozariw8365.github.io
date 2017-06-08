4abedb3ebba707f1ef8a283a8f80f2032dee8505
rozariw8365 <rozariw8365@kippnyccp.org>
2017-06-08 11:51:05 -0400

    Create application.js

---
+++ guess-number/application.js
@@ -0,0 +1,11 @@
+application.js
+$(document).ready(function(){
+	var random_num = Math.floor((Math.random()*100) + 1);
+	//create click event for submit button
+
+
+
+	//create click event for clear button
+
+
+});
